0c9128c52a09ae2cae07d5b1e9a0dfcefd6c3e1b
mido <abo.ashraf169@gmail.com>
2024-10-26 13:27:06 +0200

    Initial Commit

---
+++ SwiftfulSourseControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulSourseControl
+//
+//  Created by mido mj on 10/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulSourseControl/SwiftfulSourseControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourseControlApp.swift
+//  SwiftfulSourseControl
+//
+//  Created by mido mj on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourseControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
