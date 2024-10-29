8cb3953dc01d8276a6f7678326b1590a2ed0ff71
mido mj <abo.ashraf169@gmail.com>
2024-10-29 12:28:54 +0200

    [clean] Add commit message description

--- SwiftfulSourseControl/ContentView.swift
+++ SwiftfulSourseControl/ContentView.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+/*
+ Commit MESSAGES
+ 
+ NEW FEATURE:
+ [feature] Description of the feature
+ 
+ BUG IN PRODUCION:
+ [patch] Description of the patch
+ 
+ BUG NOT IN PRODUCION:
+ [BUG] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of the changes
+ 
+ RELEASE:
+ [release] Description of release
+
+ */
+
 struct ContentView: View {
     var body: some View {
         VStack {
