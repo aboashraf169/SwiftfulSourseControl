a0f1c8d9c5aac5d032bc69a89613ee3c3203c417
mido mj <abo.ashraf169@gmail.com>
2024-10-29 12:20:48 +0200

    Add aleart to button go to next in home view

--- SwiftfulSourseControl/AddHomeScreen.swift
+++ SwiftfulSourseControl/AddHomeScreen.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AddHomeScreen: View {
+    @State var showAlart : Bool = false
+    @State var textAleart : String  = ""
     var body: some View {
         ZStack{
             Color.brown.ignoresSafeArea()
@@ -22,7 +24,8 @@ struct AddHomeScreen: View {
                     .padding()
             
                 Button(action: {
-                    
+                    showAlart.toggle()
+                    textAleart = "Your new todo item must be at least 3 characters long!!!😨😰😱"
                 }, label: {
                     Text("go to next")
                         .font(.headline)
@@ -33,9 +36,15 @@ struct AddHomeScreen: View {
                         .cornerRadius(10)
                         .padding()
                 })
+                .alert(isPresented: $showAlart){
+                    GetAleart()
+                }
             }
-
         }
+
+    }
+    func GetAleart() -> Alert {
+            Alert(title: Text(textAleart), primaryButton:.default(Text("add")), secondaryButton: .cancel())
     }
 }
 
